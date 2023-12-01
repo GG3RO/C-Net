8754dd74dcc96fb4df78b2cf3b9b61262f8955ab
GG3RO <geris.duli@gmail.com>
2023-12-01 15:31:06 +0100

    fundamentals 1

---
+++ fundamentals1/Program.cs
@@ -0,0 +1,44 @@
+﻿
+for (int i = 1; i <= 255; i++){
+    System.Console.WriteLine(i);
+
+}
+// Random rand = new Random();
+// for (int i=1; i <=5; i++){
+//      Console.WriteLine(rand.Next(10,20));
+// }
+
+Random rand = new Random();
+int sum=0;
+for (int i=1; i <=5; i++){
+    Console.WriteLine(rand.Next(10,20));
+    sum = sum + rand.Next(10,20);
+    System.Console.WriteLine(sum);
+
+}
+
+for(int i=1; i<=100; i++){
+    if(i % 3 ==0 && i% 5 ==0){
+        ;
+    }
+    else if(i % 3 ==0){
+        System.Console.WriteLine("fizz");   //cw (i)
+    }
+        else if (i % 5 ==0) {
+
+        System.Console.WriteLine("buzz");   //cw (i)
+    }
+}
+
+for(int i=1; i<=100; i++){
+    if(i % 3 ==0 && i% 5 ==0){
+        System.Console.WriteLine("fizzBuzz");
+    }
+    else if(i % 3 ==0){
+        System.Console.WriteLine("fizz");
+    }
+        else if (i % 5 ==0) {
+            
+        System.Console.WriteLine("buzz");
+    }
+}
---
+++ fundamentals2/Program.cs
@@ -0,0 +1,23 @@
+﻿static void PrintList(List<string> MyList)
+{
+    foreach(string item in MyList){
+        System.Console.WriteLine(item);
+
+    };
+}
+List<string> TestStringList = new List<string>() {"Harry", "Steve", "Carla", "Jeanne"};
+PrintList(TestStringList);
+
+static void SumOfNumbers(List<int> IntList)
+{
+    int sum=0;
+    foreach(int number in IntList){
+        sum+= number;
+        System.Console.WriteLine(sum);
+
+    }
+    
+}
+List<int> TestIntList = new List<int>() {2,7,12,9,3};
+// You should get back 33 in this example
+SumOfNumbers(TestIntList);
