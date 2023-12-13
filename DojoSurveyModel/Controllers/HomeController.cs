b7d6a9d3a6f1d8a1792fe60100cd5b97749a2a7f
GG3RO <geris.duli@gmail.com>
2023-12-13 17:35:14 +0100

    DojoSurveyWithModel

---
+++ DojoSurveyModel/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurveyModel.Models;
+
+namespace DojoSurveyModel.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost("Results")]
+    public IActionResult Results(Survey Info ){
+        
+        
+        return View("Results" ,Info);
+
+    }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
